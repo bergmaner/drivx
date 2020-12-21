1a9f3a95645e4cf6fc1e738b32fac9ef74feec5d
bergmaner <bergkacper@gmail.com>
2020-12-21 17:32:12 +0100

    get directionData from googleDirectionAPI

--- lib/helpers/helperMethods.dart
+++ lib/helpers/helperMethods.dart
@@ -2,8 +2,10 @@ import 'package:connectivity/connectivity.dart';
 import 'package:drivx/constants.dart';
 import 'package:drivx/helpers/requestHelper.dart';
 import 'package:drivx/models/addresModel.dart';
+import 'package:drivx/models/directionDetails.dart';
 import 'package:drivx/provider/AppData.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import "package:provider/provider.dart";
 
 class HelperMethods{
@@ -29,5 +31,24 @@ class HelperMethods{
     else return ":(((((";
 
     return address;
+  }
+  static Future<DirectionDetails> getDirectionDetails(LatLng start, LatLng end) async{
+    String url = 'https://maps.googleapis.com/maps/api/directions/json?origin=${start.latitude},${start.longitude}&destination=${end.latitude},${end.longitude}&mode=driving&key=$mapKey';
+    var response = await RequestHelper.getRequest(url);
+
+    if(response == "failure"){
+      return null;
+    }
+    if(response['status'] == "OK"){
+        DirectionDetails directionDetails = DirectionDetails();
+
+        directionDetails.durationText = response["routes"][0]["legs"][0]["duration"]["text"];
+        directionDetails.durationValue = response["routes"][0]["legs"][0]["duration"]["value"];
+        directionDetails.distanceText = response["routes"][0]["legs"][0]["distance"]["text"];
+        directionDetails.distanceValue = response["routes"][0]["legs"][0]["distance"]["value"];
+        directionDetails.encodedPoints = response["routes"][0]["overview_polyline"]["points"];
+
+        return directionDetails;
+    }
   }
 }
